910ca14d000ae465b3324b0fc0e2853adacbd537
katsuhiko <katsuhiko.nagashima@gmail.com>
2026-01-13 00:04:33 +0900

    PHPStan, CodeSniffer が動作するように変更しました。

--- cms/src/View/AjaxView.php
+++ cms/src/View/AjaxView.php
@@ -20,6 +20,8 @@ namespace App\View;
  * A view class that is used for AJAX responses.
  * Currently only switches the default layout and sets the response type -
  * which just maps to text/html by default.
+ *
+ * @extends AppView<self>
  */
 class AjaxView extends AppView
 {
