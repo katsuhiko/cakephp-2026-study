a47f4ee01a7482c615f4617cc65a471e17623f82
katsuhiko <katsuhiko.nagashima@gmail.com>
2026-01-12 19:29:40 +0900

    Sqlite3を利用する設定とSchema・データを追加しました

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM php:8.4-cli
 
 RUN apt-get update
 
-RUN apt-get install -y unzip
+RUN apt-get install -y unzip sqlite3
 
 RUN apt-get install -y libicu-dev \
     && docker-php-ext-install intl
--- cms/config/app_local.example.php
+++ cms/config/app_local.example.php
@@ -36,7 +36,7 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'localhost',
+            // 'host' => 'localhost',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -44,10 +44,10 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
-            'password' => 'secret',
+            // 'username' => 'my_app',
+            // 'password' => 'secret',
 
-            'database' => 'product_myapp',
+            // 'database' => 'product_myapp',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
@@ -64,11 +64,11 @@ return [
          * The test connection is used during the test suite.
          */
         'test' => [
-            'host' => 'localhost',
+            // 'host' => 'localhost',
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'test_myapp',
+            // 'username' => 'my_app',
+            // 'password' => 'secret',
+            // 'database' => 'test_myapp',
             //'schema' => 'myapp',
             'url' => env('DATABASE_TEST_URL', 'sqlite://127.0.0.1/tmp/tests.sqlite'),
         ],
